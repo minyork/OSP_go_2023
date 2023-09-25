639bd289ba1307ccd6a0dea03032958ba63f1d90
minyork <minyork0312@gmail.com>
2023-09-25 10:39:42 +0900

    v1.1

--- week05/main.go
+++ week05/main.go
@@ -9,6 +9,8 @@ func main() {
 	var now time.Time = time.Now()
 	var year int = now.Year() // 위 변수 now 가져옴.
 	fmt.Println(year)
-	fmt.Println(now.Month(), now.Minute(), now.Second())
+	// fmt.Println(now.Month(), now.Minute(), now.Second())
 
+	month := now.Month()
+	fmt.Println(month)
 }
